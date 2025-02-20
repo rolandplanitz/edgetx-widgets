16ee0688f578352a2fd50f7e114cc1f50ceac8d4
Daniel Barrios <ing.dbarrios@gmail.com>
2025-02-20 13:57:23 +0100

    Sim model Ajusted

--- widgets/SimModel/main.lua
+++ widgets/SimModel/main.lua
@@ -1,4 +1,7 @@
--- Define positions
+-- Widget LUA Script: SimModel Widget
+-- Displays model information, battery voltage, and date/time
+
+-- Define positions and styles
 local xLeft = 10
 local yStart = 5
 local lineHeight = 18
@@ -7,7 +10,7 @@ local textStyle = WHITE + LEFT + SHADOWED
 local textStyleRight = RIGHT + WHITE + SHADOWED
 
 local idTxV
-  
+
 -- Variables used across all instances
 local vcache -- valueId cache
 local mod = {} -- module info
@@ -33,7 +36,6 @@ local function create(zone, options)
   idTxV = getFieldInfo('tx-voltage').id
   local _, rv = getVersion()
 
-
   return widget
 end
 
@@ -42,55 +44,49 @@ local function update(widget, options)
   widget.cfg = options
 end
 
-local function drawModelInfo()
+local function drawModelInfo(widget)
+  -- Get model name from options or use default
+    local modelName = model.getInfo().name
 
-  -- Get model name
-  local modelName = "dbarrios83"
-
-	-- Draw Model Name
+  -- Draw Model Name
   lcd.drawText(xLeft, yStart, modelName, textStyle + MIDSIZE + BOLD)
   
-    -- o/s version
-	local _, _, major, minor, rev, osname = getVersion()
-	strVer = "Simulator. " .. (osname or "EdgeTX") .. " " .. major .. "." .. minor.. "." .. rev
+  -- o/s version
+  local _, _, major, minor, rev, osname = getVersion()
+  local strVer = "Simulator. " .. (osname or "EdgeTX") .. " " .. major .. "." .. minor .. "." .. rev
 
-  lcd.drawText(xLeft, yStart + 2*lineHeight, strVer, textStyle + SMLSIZE)
-
+  lcd.drawText(xLeft, yStart + 2 * lineHeight, strVer, textStyle + SMLSIZE)
 end
 
 -- Function to draw the battery icon based on voltage
 local function drawBattery()
-
   idTxV = getFieldInfo('tx-voltage').id
   local vBatt = tonumber(getValue(idTxV)) or 0
-  lcd.drawText(xLeft , yStart + 8 + 3*lineHeight, string.format("%.1fV", vBatt), textStyle + MIDSIZE)
+  lcd.drawText(xLeft, yStart + 8 + 3 * lineHeight, string.format("%.1fV", vBatt), textStyle + MIDSIZE)
   
   -- Determine battery icon based on RXBt vBatt
-	local iconPath = "/WIDGETS/SimModel/BMP/battery-%s.png"
-	local iconState
+  local iconPath = "/WIDGETS/SimModel/BMP/battery-%s.png"
+  local iconState
   if vBatt < 7.1 then
-		iconState = "low"
-	elseif vBatt < 7.5 then
-		iconState = "yellow"
-	elseif vBatt < 7.9 then
-		iconState = "ok"
-	else
-		iconState = "full"
-	end
-	local icon = Bitmap.open(string.format(iconPath, iconState))
+    iconState = "low"
+  elseif vBatt < 7.5 then
+    iconState = "yellow"
+  elseif vBatt < 7.9 then
+    iconState = "ok"
+  else
+    iconState = "full"
+  end
+  local icon = Bitmap.open(string.format(iconPath, iconState))
 
-	-- Draw battery icon
-	lcd.drawBitmap(icon, xLeft + 55 , yStart + 5 + 3*lineHeight)
-
-
+  -- Draw battery icon
+  lcd.drawBitmap(icon, xLeft + 55, yStart + 5 + 3 * lineHeight)
 end
 
 local function drawDateAndTime(widget)
-
   local datetime = getDateTime()
   if not datetime then
-      lcd.drawText(widget.zone.x + 10, widget.zone.y + 10, "No Date/Time", MIDSIZE + RED)
-      return
+    lcd.drawText(widget.zone.x + 10, widget.zone.y + 10, "No Date/Time", MIDSIZE + RED)
+    return
   end
 
   -- Convert month to 3-letter abbreviation
@@ -105,27 +101,20 @@ local function drawDateAndTime(widget)
   local yStart = widget.zone.y + 5
 
   -- Draw Date and Time Block
-  lcd.drawText(xRight, yStart, timeStr, textStyleRight + MIDSIZE)
-  lcd.drawText(xRight, yStart + midLineHeight, dateStr, textStyleRight)
-
+  lcd.drawText(xRight, yStart, timeStr, textStyleRight)
+  lcd.drawText(xRight, yStart + lineHeight, dateStr, textStyleRight)
 end
 
 local function refresh(widget, event, touchState)
-
-  drawModelInfo()
-
+  drawModelInfo(widget)
   drawBattery()
-
   drawDateAndTime(widget)
-
 end
-
 
 return {
   name = "SimModel",
   options = {},
   create = create,
   update = update,
-  refresh = refresh,
-  options = {}
+  refresh = refresh
 }
