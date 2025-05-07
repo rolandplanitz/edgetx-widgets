a6a817ab98b2a858b8a4e1e97549595b367004c6
Daniel Barrios <ing.dbarrios@gmail.com>
2025-05-07 21:16:28 +0200

    Optimized Clock widget

--- widgets/ClockWidget/main.lua
+++ widgets/ClockWidget/main.lua
@@ -1,23 +1,13 @@
--- Basic LUA Script with Date and Time Widget
--- Displays the current date and time with a modified date format
+-- ClockWidget: Displays current date and time with telemetry-based connection icon
+
+-- Constants
 local textStyle = RIGHT + WHITE + SHADOWED
-local midLineHeight = 25
 local lineHeight = 18
+local months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+                "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
+local iconPathFmt = "/WIDGETS/ClockWidget/BMP/connection-%s.png"
 
--- Function to create the widget
-local function create(zone, options)
-    return {
-        zone = zone,
-        options = options,
-    }
-end
-
--- Function to update the widget configuration
-local function update(widget, options)
-    widget.options = options
-end
-
--- Function to get the formatted time
+-- Function to format time string
 local function getFormattedTime(datetime, is24Hour)
     local hour = datetime.hour
     local amPm = ""
@@ -39,9 +29,29 @@ local function getFormattedTime(datetime, is24Hour)
     return string.format("%02d:%02d%s", hour, datetime.min, is24Hour and "" or amPm)
 end
 
--- Function to refresh the widget display
+-- Widget creation: preload bitmaps
+local function create(zone, options)
+    local icons = {
+        red = Bitmap.open(string.format(iconPathFmt, "red")),
+        yellow = Bitmap.open(string.format(iconPathFmt, "yellow")),
+        green = Bitmap.open(string.format(iconPathFmt, "green")),
+        black = Bitmap.open(string.format(iconPathFmt, "black")),
+    }
+
+    return {
+        zone = zone,
+        options = options,
+        icons = icons,
+    }
+end
+
+-- Widget config update
+local function update(widget, options)
+    widget.options = options
+end
+
+-- Main drawing function
 local function refresh(widget, event, touchState)
-    -- Get the current date and time
     local datetime = getDateTime()
     if not datetime then
         lcd.drawText(widget.zone.x + 10, widget.zone.y + 10, "No Date/Time", MIDSIZE + RED)
@@ -49,72 +59,45 @@ local function refresh(widget, event, touchState)
     end
 
     local tpwr = tonumber(getValue("TPWR")) or 0
-
     local iconState
-    -- Determine battery icon based on RXBt value
-    local iconPath = "/WIDGETS/ClockWidget/BMP/connection-%s.png"
 
-        
-	if tpwr > 0 then
-
-		-- Get telemetry data
+    if tpwr > 0 then
         local rqly = tonumber(getValue("RQly")) or 0
-
-		if rqly < 60 then
-			iconState = "red"
-		elseif rqly < 80 then
-			iconState = "yellow"
+        if rqly < 60 then
+            iconState = "red"
+        elseif rqly < 80 then
+            iconState = "yellow"
         else
             iconState = "green"
-		end
-
+        end
     else
         iconState = "black"
-	end
-
-
-    -- Convert month to 3-letter abbreviation
-    local months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
-    local monthStr = months[datetime.mon] or "???"
-
-    -- Determine time format
-    local is24Hour = widget.options.Format24H == 1 -- Check the TimeFormat option
-
-    -- Get the formatted time string
-    local timeStr = getFormattedTime(datetime, is24Hour)
-    local dateStr = string.format("%02d %s", datetime.day, monthStr)
-
-    -- Define positions
-    local xRight = widget.zone.x + widget.zone.w - 10
-    local yStart = widget.zone.y + 5
-
-    -- Draw connection icon
-    local iconRight
-    if is24Hour then
-        iconRight = xRight - 85
-    else
-        iconRight = xRight - 110
     end
 
-     
+    local is24Hour = widget.options.Format24H == 1
+    local timeStr = getFormattedTime(datetime, is24Hour)
+    local dateStr = string.format("%02d %s", datetime.day, months[datetime.mon] or "???")
 
+    local xRight = widget.zone.x + widget.zone.w - 10
+    local yStart = widget.zone.y + 5
+    local iconRight = xRight - (is24Hour and 85 or 110)
 
-    local icon = Bitmap.open(string.format(iconPath, iconState))
-    lcd.drawBitmap(icon, iconRight, yStart + 2)
+    local icon = widget.icons[iconState]
+    if icon then
+        lcd.drawBitmap(icon, iconRight, yStart + 2)
+    end
 
-
-    -- Draw Date and Time Block
     lcd.drawText(xRight, yStart, timeStr, textStyle)
     lcd.drawText(xRight, yStart + lineHeight, dateStr, textStyle)
 end
 
+-- Return widget definition
 return {
     name = "ClockWidget",
-    options = {},
     create = create,
     update = update,
     refresh = refresh,
     options = {
-        {"Format24H", BOOL, 1}, -- 1 for 24-hour, 0 for 12-hour
-      },
+        {"Format24H", BOOL, 1}, -- 1 = 24-hour, 0 = 12-hour
+    },
 }
